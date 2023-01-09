6e373aa1b4bf5cd4f4374288c0a18427942ae8a5
kchihogi <kchihogi@gmail.com>
2023-01-09 20:59:54 +0900

    ローカルでサーバー接続

--- app/mikado_line_bot_site/line_bot/line_message.py
+++ app/mikado_line_bot_site/line_bot/line_message.py
@@ -6,13 +6,6 @@ import urllib.request
 # from django.http import HttpResponse
 # from django.views.decorators.csrf import csrf_exempt
 
-REPLY_ENDPOINT_URL = "https://api.line.me/v2/bot/message/reply"
-ACCESSTOKEN = '***先ほど発行したアクセストークンをここに貼り付け***'
-HEADER = {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer ' + ACCESSTOKEN
-}
-
 class LineMessage():
     """Controler of a line message.
     """
@@ -25,19 +18,20 @@ class LineMessage():
         Args:
             reply_token (_type_): token from the line host server.
         """
-        body = {
-            'replyToken': reply_token,
-            'messages': self.messages
-        }
-        print(body)
-        req = urllib.request.Request(REPLY_ENDPOINT_URL, json.dumps(body).encode(), HEADER)
-        try:
-            with urllib.request.urlopen(req) as res:
-                body = res.read()
-        except urllib.error.HTTPError as err:
-            print(err)
-        except urllib.error.URLError as err:
-            print(err.reason)
+        print("Hello World.")
+        # body = {
+        #     'replyToken': reply_token,
+        #     'messages': self.messages
+        # }
+        # print(body)
+        # req = urllib.request.Request(REPLY_ENDPOINT_URL, json.dumps(body).encode(), HEADER)
+        # try:
+        #     with urllib.request.urlopen(req) as res:
+        #         body = res.read()
+        # except urllib.error.HTTPError as err:
+        #     print(err)
+        # except urllib.error.URLError as err:
+        #     print(err.reason)
 
     def dummy(self):
         """dummy method.
--- app/mikado_line_bot_site/line_bot/views.py
+++ app/mikado_line_bot_site/line_bot/views.py
@@ -6,19 +6,44 @@ import json
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 
-from utils import message_creater
-from line_bot.line_message import LineMessage
+
+from django.conf import settings
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+# from utils import message_creater
+# from line_bot.line_message import LineMessage
+
+line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(settings.LINE_CHANNEL_SECRET)
 
 @csrf_exempt
 def index(request):
     """index page.
     """
+    # get X-Line-Signature header value
     if request.method == 'POST':
-        request = json.loads(request.body.decode('utf-8'))
-        data = request['events'][0]
-        message = data['message']
-        reply_token = data['replyToken']
-        line_message = LineMessage(message_creater.create_single_text_message(message['text']))
-        line_message.reply(reply_token)
+        signature = request.headers['X-Line-Signature']
+        body = request.body.decode(request.encoding)
+        # handle webhook body
+        try:
+            handler.handle(body, signature)
+        except InvalidSignatureError:
+            print("Invalid signature. Please check your channel access token/channel secret.")
+            return HttpResponseBadRequest
         return HttpResponse("ok")
     return HttpResponseBadRequest
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
